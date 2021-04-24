785e5552adba429c5308dfdf927b083eee182480
Nekomata <67730211+nixneko@users.noreply.github.com>
2021-04-24 18:58:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from threading import Thread
 
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask('')
+
+
+@app.route("/", methods=["GET"])
 
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.debug = True
+def get_my_ip():
+    x = request.remote_addr
+    file = open('text.log', 'a')
+    file.write(f'{x}\n')
+    return x
+
+def run():
     app.run(host="0.0.0.0", port="6969")
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
